bf0fd8c5e09fd499e894df37dfb96c25b358c9a5
yusufmistry <112641407+yusufmistry@users.noreply.github.com>
2024-09-13 18:48:06 +0530

    port

--- server.js
+++ server.js
@@ -49,5 +49,5 @@ mongoose.connect('mongodb+srv://yamboygenius:Binduk%40123@cluster0.j5u1mcp.mongo
 
 
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running sucessfully on PORT ${PORT}`))
